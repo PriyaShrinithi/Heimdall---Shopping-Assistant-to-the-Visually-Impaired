65c0428d7f3c4986eab35479c25337811faa769d
Priya Shrinithi <47693475+PriyaShrinithi@users.noreply.github.com>
2021-02-14 21:25:18 +0530

    Add files via upload

--- predict_app/predict_app.py
+++ predict_app/predict_app.py
@@ -16,6 +16,12 @@ from PIL import Image
 import random
 
 from tensorflow.python.keras.models import load_model
+import keras.backend as K
+from keras.preprocessing.image import ImageDataGenerator
+from keras import layers, models, optimizers, losses, Sequential
+from keras.preprocessing.image import img_to_array, load_img
+from keras.applications.vgg16 import VGG16
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard, EarlyStopping
 from tqdm import tqdm  # to increase iteration speed
 import gc
 import matplotlib.pyplot as plt
@@ -42,26 +48,32 @@ app = Flask(__name__)
 # __name__: built in variable; evaluates the name of the current module
 CORS(app)
 
+model = []
+prediction_distr = []
+graphs = []
 
-def get_model():  # a function to load the model
-    global model  # model can be accessed throughout the app
-    model = load_model('D:\Freidburg-s-Groceries\h5 files\groceries_2.h5', compile=False)
-    # we choose the model groceries_2.h5 present in the same directory
-    # why Adam? faster than sdg and uses sdg extensions
-    # why learning rate 10e-4?
-    print("model loaded!")
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+
+def get_model(i):  # a function to load the model
+    if i < 7:
+         # model can be accessed throughout the app
+        m = load_model('extracted_path_of_h5 files_directory\h5 files\groceries_'+str(i+1)+'.h5', compile=False)
+        print("model "+ str(i+1)+" loaded!")
+        graph = tf.get_default_graph()
+        return m, graph;
 
 
-'''def read_class():  # a mapof the encoded target with its corresponding class
-    global class_dictionary  # can be seen throughout the app
-    with open('class_map.txt', 'r') as file:
-        class_dictionary = file.read()
-    class_dictionary = eval(class_dictionary)
+def read_class(i):  # a mapof the encoded target with its corresponding class
+    with open('extracted_path_of_class_maps_directory\class_maps\class_map_'+str(i+1)+'.txt', 'r') as file:
+        c = file.read()
+    c = eval(c)
+    return c
     # takes content from txt file (created in groceies.ipynb) and converts to dictionary
-'''
-
 
 def preprocess_image(image, target_size):
+    image = Image.open(image)
     if image.mode != 'RGB':  # if the image is not in rgb, convert to rgb
         image = image.convert('RGB')
     image = image.resize(target_size)
@@ -71,17 +83,28 @@ def preprocess_image(image, target_size):
     # basically for axis = 0 changes shape from (n,n) to (1, n, n)
     # for axis = 1, shape change follows (n, n) to (n, 1 , n)
 
+def looping_model_and_class():
+    for i in range(7):
+        print("Loading Keras Models...")
+        gc.collect()
+        m,g = get_model(i)
+        model.append(m);
+        graphs.append(g)
+    print("Models loaded")
 
-print("Loading Keras Model")
-gc.collect()  # essential for smooth running of the model
-get_model()
-#read_class()
-
+looping_model_and_class()
+ # essential for smooth running of the model
+def prediction_logic(i, image):
+    m = model[i]
+    g = graphs[i]
+    with g.as_default():
+        pred_distribution = m.predict(image)
+        pred_distribution = np.argmax(pred_distribution)
+    return pred_distribution
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
 
 # the flask app can be accessed by host/upload
 # nature of the app is: post
@@ -92,4 +115,13 @@ def predict():
     f.save(
         os.path.join('uploads', secure_filename(f.filename)))  # save image as file in uploads directory in the server
     image = os.path.join('uploads', str(f).split(' ')[1].split('\'')[1])
-    image = preprocess_imag
+    image = preprocess_image(image, (100, 100))
+    for i in range(7):
+        p = prediction_logic(i, image)
+        prediction_distr.append(p)
+    pred = np.asarray(prediction_distr)
+    p = np.argmax(pred)
+    i = list(pred).index(p)
+    class_dictionary = read_class(i)
+    print(class_dictionary[p])
+    return jsonify(class_dictionary[p])
